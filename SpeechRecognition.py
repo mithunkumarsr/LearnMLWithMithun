ce36f97d164e168317ff225b61ffdd29669b7881
Mithun Kumar S R <mithunkumar.sr@uber.com>
2020-12-20 15:48:07 +0530

    Update SpeechRecognition.py

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    print ("speak anything..")
+    print ("Speak anything..")
     audio = r.listen(source)
 
     try:
